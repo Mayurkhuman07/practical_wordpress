8d3d3c1e95df088b2bd617f3b35776d0690d5c28
Mayur khuman <55486930+Mayurkhuman07@users.noreply.github.com>
2023-07-15 15:44:28 +0530

    first commit
    first commit

---
+++ wordpressproject/wp-content/themes/astra-child/functions.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra Child
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
+require_once( get_stylesheet_directory() . '/class-wp_wc_practical.php' );
+
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+
+// Enqueue Main.js File
+function enqueue_custom_scripts() {
+    wp_enqueue_script('custom-main-js', get_template_directory_uri() . '/main.js', array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
+
+
+
+add_filter ('add_to_cart_redirect', 'redirect_to_checkout');
+
+function redirect_to_checkout() {
+    global $woocommerce;
+    $checkout_url = $woocommerce->cart->get_checkout_url();
+    return $checkout_url;
+}
+
+
+
+////shortcode/////////
+
